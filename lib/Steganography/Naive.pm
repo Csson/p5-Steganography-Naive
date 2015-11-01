e464e1cb33a5b867b69afa3da2ae4d9342ffdc73
Erik Carlsson <info@code301.com>
2015-11-01 11:55:05 +0000

    Init

---
+++ lib/Steganography/Naive.pm
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+
+package Steganography::Naive;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Steganography::Naive;
+
+=head1 DESCRIPTION
+
+Steganography::Naive is ...
+
+=head1 SEE ALSO
+
+=cut
